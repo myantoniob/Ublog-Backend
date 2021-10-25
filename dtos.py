1e6f428890a7e67e61f121578d042c191620737e
Mynor Ramirez <30330796+myantoniob@users.noreply.github.com>
2021-10-25 09:23:34 -0600

    .
    .

--- dtos.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, first_name, last_name, date, gender, nickname, password, phone):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.date = date
-        self.gender = gender
-        self.nickname = nickname
-        self.password = password
-        self.phone = phone
